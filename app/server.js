04de92a48b53fd8493de16534e5f50fbd2cb335f
Pavlo Yuriychuk <pluccky@gmail.com>
2014-03-29 17:20:48 +0200

    Fixed mistakes in code. Added more logging to server. Fixed mistakes in template.

--- app/server.js
+++ app/server.js
@@ -14,29 +14,35 @@ var logger = Logger.getInstance("feed-server", config.logPath);
 
 var App = {};
 App.init = function () {
+    logger.info("Loading template");
     var filePath = path.join(__dirname , 'views', 'main.hbs');
     return readFile(filePath, 'utf-8');
 };
 
 App.fetch = function () {
+    logger.info("Loading data");
     return Q.all(_.map(config.feeds, function (item) {
         return readFile(path.join(config.dataPath, item.name + ".json"), "utf-8");
     }));
 };
 
 App.run = function (template) {
+    logger.info("Loaded template");
     App.template = Handlebars.compile(template);
     App.server = http.createServer(function(request, response) {
         App.fetch().then(function (data) {
-            logger.debug(data);
+            logger.info("Loaded data");
             var feed = {
                 title: "Newsfeed reader"
             };
-            feed.feeds = _.map(data, function (item) {
-                return JSON.parse(item);
+            feed.feeds = _.map(data, function (item, index) {
+                return {
+                    title: config.feeds[index].title,
+                    posts: JSON.parse(item)
+                };
             });
             response.writeHead(200, {"Content-Type": "text/html"});
-            response.write(this.htmlTemplate(feed));
+            response.write(App.template(feed));
             response.end();
         });
     }).listen(config.port);
